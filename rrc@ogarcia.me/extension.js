f808d84ac8066599a9d56480ecd340a5153526aa
Óscar García Amor <ogarcia@connectical.com>
2021-05-04 16:18:12 +0200

    Remove actor to avoid deprecation warning

--- rrc@ogarcia.me/extension.js
+++ rrc@ogarcia.me/extension.js
@@ -1,10 +1,10 @@
 const Main = imports.ui.main;
 function init() {}
 function enable() {
-  Main.panel._leftCorner.actor.add_style_class_name('no-radius');
-  Main.panel._rightCorner.actor.add_style_class_name('no-radius');
+  Main.panel._leftCorner.add_style_class_name('no-radius');
+  Main.panel._rightCorner.add_style_class_name('no-radius');
 }
 function disable() {
-  Main.panel._leftCorner.actor.remove_style_class_name('no-radius');
-  Main.panel._rightCorner.actor.remove_style_class_name('no-radius');
+  Main.panel._leftCorner.remove_style_class_name('no-radius');
+  Main.panel._rightCorner.remove_style_class_name('no-radius');
 }
