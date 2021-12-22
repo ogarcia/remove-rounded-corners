6b7572e9939721f7bbd2122805871faaf13a25fa
Óscar García Amor <ogarcia@connectical.com>
2021-12-22 17:22:41 +0100

    Make extension more tiny

--- rrc@ogarcia.me/extension.js
+++ rrc@ogarcia.me/extension.js
@@ -1,8 +1,2 @@
-const Main = imports.ui.main;
-function init() {}
-function enable() {
-  Main.layoutManager.uiGroup.add_style_class_name("remove-rounded-corners");
-}
-function disable() {
-  Main.layoutManager.uiGroup.remove_style_class_name("remove-rounded-corners");
-}
+function enable() {}
+function disable() {}
